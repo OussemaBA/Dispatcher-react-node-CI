1d839f6c4a5be4449e77be13ba8f9791590997fd
Oussema <ghost@pop-os.localdomain>
2020-05-22 20:55:04 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//express=collection of function (helpers)
+// that ease writing servers .we could build it from scratch
+const express=require("express");
+const app=express();
+
+
+app.use((req,res)=>{
+    res.send("hi: Hello");
+})
+
+
+
+const PORT=process.env.PORT || 5000;
+
+app.listen(5000);
+
+
+
+
